51ce0fc5bc3db7e3172447fec7c7c8005e6fcfd4
Steve Saylor <steven.saylor@pitt.edu>
2022-09-23 14:41:48 -0400

    fixed debug env handling

--- api/housecat/settings.py
+++ api/housecat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', 'False').lower() in ['true', '1']
 
 ALLOWED_HOSTS = json.loads(os.environ.get('ALLOWED_HOSTS', '["www.housecatpgh.org", "localhost"]'))
 
