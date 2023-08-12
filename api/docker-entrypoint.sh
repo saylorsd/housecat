b21ce4ef417b700ca04afe61ce9f50cce2252b5b
Tyrica Terry Kapral <tyt3@pitt.edu>
2023-08-11 22:58:44 -0400

    Update Docker files

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 # Apply database migrations
 echo "Apply database migrations"
 ./bin/wait-for-it.sh -t 5 db:5432 -- echo "✅ DB is up"
+./bin/wait-for-it.sh -t 5 datastore-db:5432 -- echo "✅ Datastore DB is up"
 
 COUNTER=0
 
@@ -12,7 +13,7 @@ while
   [[ $M -eq 1 ]] && [ $COUNTER -lt 6 ]
 do
   ((COUNTER++))
-  echo "⚠️ couldn't migrate, tyring again shortly"
+  echo "⚠️ couldn't migrate, trying again shortly"
   echo "    (attempt $COUNTER of 5)"
   sleep 3
 done
@@ -25,6 +26,11 @@ echo "📡 Connecting to CKAN datastore"
 
 echo "🚛 Collecting Housing Data"
 ./manage.py connect_housing_data
+
+if [[ -v $MANAGE_DATASTORE ]]; then
+  echo "🧪🗄️ Generating Dummy Data"
+  ./manage.py generate_dummy_data
+fi
 
 echo "📦 Collecting static files"
 ./manage.py collectstatic --noinput
